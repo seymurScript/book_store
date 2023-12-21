6c7706a89508e1561576676870ff7a0d92532c37
seymurScript <seymur2519@gmail.com>
2023-12-22 02:50:24 +0400

    configuration firebase and write read data on admin page

--- js/catalog.js
+++ js/catalog.js
@@ -2,13 +2,17 @@ const readMoreBtns=document.querySelectorAll('.readBtn');
 const catalogStage=document.querySelector('.catalogMain');
 const readPageStage= document.querySelector('.readPage')
 const backBtn=document.querySelector('.backBtn')
-
-
-
+const commitInput=document.querySelector('.commit-input')
+const sendBtn=document.querySelector('.sendBtn')
+var commitAPI=`https://blog-api-t6u0.onrender.com/posts`
+var currentTime = moment().format('MMMM Do YYYY, HH:mm:ss');
 // readMoreBtns.addEventListener('click',closeFirstSection)
 backBtn.addEventListener('click',closeSecondSection)
-
-
+sendBtn.addEventListener('click',sendCommit)
+var commitData={
+    title:'salam',
+    body:'sakamnfdsf'
+}
 function closeFirstSection(){
     catalogStage.style.display='none'
     readPageStage.style.display='block'
@@ -20,3 +24,15 @@ function closeSecondSection(){
 readMoreBtns.forEach(function(button) {
     button.addEventListener('click', closeFirstSection);
   });
+// function sendCommit(){
+//     if(commitInput.value.trim()!==''){
+//         fetch(commitAPI, {
+//             // method: 'DELETE',
+//             // body: commitData
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//         })
+//     }
+// }
